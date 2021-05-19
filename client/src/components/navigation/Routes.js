b5d8453b44e440341ef820a5874dfb9afc1ae239
Ilari Junnonen <ilari.junnonen@iki.fi>
2021-05-19 19:50:21 +0300

    Change LoginForm to LoginPage in Routes

--- client/src/components/navigation/Routes.js
+++ client/src/components/navigation/Routes.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Switch from "react-router-dom/Switch";
 import Route from "react-router-dom/Route";
-import LoginForm from "../LoginForm";
+import LoginPage from "../LoginPage";
 
 const Routes = () => (
   <Switch>
-    <Route exact path="/" component={LoginForm}></Route>
+    <Route exact path="/" component={LoginPage}></Route>
     <Route path="/add">Add</Route>
     <Route path="/products">Products</Route>
     <Route path="/orders">Orders</Route>
