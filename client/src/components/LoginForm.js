785f9b6e1a68a54f33f363e55a343844dd9d635f
NinaWelch <nwelch1@live.co.uk>
2021-05-19 16:43:18 +0300

    add login page

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,20 +1,50 @@
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const LoginForm = () => {
   return (
-    <Col>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Col>
+    <Form
+      as={Col}
+      xs={12}
+      sm={{ span: 10, offset: 1 }}
+      md={{ span: 8, offset: 2 }}
+      lg={{ span: 6, offset: 3 }}
+      xl={{ span: 4, offset: 4 }}
+    >
+      <Form.Label as="h3" className="my-4 text-center">
+        Kirjaudu palveluun
+      </Form.Label>
+      <Form.Group>
+        <Form.Control
+          size="lg"
+          type="email"
+          placeholder="Sähköposti"
+          className="mb-3"
+        />
+
+        <Form.Control
+          size="lg"
+          type="password"
+          placeholder="Salasana"
+        />
+      </Form.Group>
+      <Form.Row className="mb-3">
+        <Col>
+          <Button
+            style={{ width: "100%" }}
+            variant="success"
+            size="lg"
+            type="submit"
+          >
+            Kirjaudu sisään
+          </Button>
+        </Col>
+      </Form.Row>
+      <div>
+        <a href="/">Salasana unohtunut?</a>
+      </div>
+    </Form>
   );
 };
 
---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,12 @@
+import Row from 'react-bootstrap/Row';
+import LoginForm from "./LoginForm";
+
+const LoginPage = () => {
+  return (
+    <Row>
+      <LoginForm />
+    </Row>
+  );
+};
+
+export default LoginPage;
