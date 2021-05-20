3f31e6f6fb4b04d157f31ec25302aefeb3fcba20
NinaWelch <nwelch1@live.co.uk>
2021-05-20 22:04:19 +0300

    Add Buyer Profile

--- client/src/components/navigation/Routes.js
+++ client/src/components/navigation/Routes.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import LoginPage from "../LoginPage";
+import ProfilePageBuyer from "../ProfilePageBuyer";
 
 const Routes = () => (
   <Switch>
-    <Route exact path="/" component={LoginPage}></Route>
+    <Route exact path="/" component={LoginPage} />
     <Route path="/add">Add</Route>
     <Route path="/products">Products</Route>
     <Route path="/orders">Orders</Route>
-    <Route path="/profile">Profile</Route>
+    <Route path="/profile" component={ProfilePageBuyer} />
     <Route>Not found</Route>
   </Switch>
 );
