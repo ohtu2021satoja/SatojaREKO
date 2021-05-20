f27c083a86ada30f7e38476b1844ec79491bc19f
Topias <topias.tallberg@gmail.com>
2021-05-20 15:52:31 +0300

    skeleton product page

--- client/src/components/navigation/Routes.js
+++ client/src/components/navigation/Routes.js
@@ -2,12 +2,13 @@ import React from "react";
 import Switch from "react-router-dom/Switch";
 import Route from "react-router-dom/Route";
 import LoginForm from "../LoginForm";
+import Products from "../Products";
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={LoginForm}></Route>
     <Route path="/add">Add</Route>
-    <Route path="/products">Products</Route>
+    <Route path="/products" component={Products}></Route>
     <Route path="/orders">Orders</Route>
     <Route path="/profile">Profile</Route>
     <Route>Not found</Route>
