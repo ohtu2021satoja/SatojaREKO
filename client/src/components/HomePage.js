dcfd588ffad6a2affce22ad0ee14c1560c42c865
Ilari Junnonen <ilari.junnonen@iki.fi>
2021-05-21 14:44:58 +0300

    Logout button and hide navbar if no user

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,3 +1,11 @@
-const HomePage = () => <h1>Home Page</h1>;
+import Button from "react-bootstrap/Button";
 
+const HomePage = ({ logOut }) => (
+  <div>
+    <h1>Home Page</h1>
+    <Button onClick={logOut} variant="danger" size="lg" type="submit">
+      Kirjaudu ulos
+    </Button>
+  </div>
+);
 export default HomePage;
