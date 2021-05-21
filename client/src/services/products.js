0c2912ce1bf8ef2d5081a31178b8103984d72272
Ilari Junnonen <ilari.junnonen@iki.fi>
2021-05-21 16:16:50 +0300

    Add axios services for users and products

---
+++ client/src/services/products.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const apiUrl = "/api/products";
+
+const getUserProducts = (id) => {
+  const request = axios.get(`${apiUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
+const getProducts = () => {
+  const request = axios.get(`${apiUrl}`);
+  return request.then((response) => response.data);
+};
+
+export default { getUserProducts, getProducts };
