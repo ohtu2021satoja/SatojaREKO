4db9bc2d9ba6b8c34dd0a5be5f810ebdfbc2df90
ToxPuro <puro.touko2@gmail.com>
2021-05-26 17:25:16 +0300

    endpoint works

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -25,7 +25,7 @@ productsRouter.get('/:id', async (req, res) => {
 
 productsRouter.post('/:id', async (req, res) => {
   const { id } = req.params
-  const dbParams = [req.body.title, req.body.organic, req.body.sellers_id, req.body.type, req.body.batch_quantity, req.body.description, req.body.imageURL, req.body.category]
+  const dbParams = [req.body.name, req.body.organic, req.body.sellers_id, req.body.type, req.body.batch_quantity, req.body.description, req.body.imageURL, req.body.category]
   await db.query("INSERT INTO products VALUES(DEFAULT, $1, $2, $3, $4, $5, DEFAULT, $6, $7, $8)", dbParams)
 })
 
