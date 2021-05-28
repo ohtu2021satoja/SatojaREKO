444434ddfbff2076fa4d0f705515c7996c58f6e8
NinaWelch <nwelch1@live.co.uk>
2021-05-28 10:34:15 +0300

    get products form api

--- client/src/services/products.js
+++ client/src/services/products.js
@@ -1,31 +1,8 @@
-/*import axios from "axios"
+import axios from "axios"
 
 const apiUrl = "/api/products"
 
 export const getProducts = async () => {
   const response = await axios.get(apiUrl)
   return response.data
-}
-*/
-
-const products = [
-  {
-    id: 1,
-    category: "fruit",
-    type: "strawberry",
-  },
-  {
-    id: 2,
-    category: "fruit",
-    type: "blueberry",
-  },
-  {
-    id: 3,
-    category: "mushrooms",
-    type: "shitake",
-  },
-]
-
-export const getProducts = () => {
-  return products
 }
