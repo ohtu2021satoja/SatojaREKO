5e6ce8c3dbc6545f9c6e3da84e49493abee98ddd
Topias <topias.tallberg@gmail.com>
2021-05-28 11:02:01 +0300

    tests also work now

--- client/src/components/OrdersSellerProducts.test.js
+++ client/src/components/OrdersSellerProducts.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
-import OrdersSeller from "./OrdersSeller"
+import OrdersSellerProducts from "./OrdersSellerProducts"
 
 describe("<Products> component", () => {
   test("renders text", () => {
-    const component = render(<OrdersSeller />);
+    const component = render(<OrdersSellerProducts />);
     expect(component.container).toHaveTextContent("Tilaukset");
   });
 });
