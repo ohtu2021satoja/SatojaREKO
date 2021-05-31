f0450777cd14915693a8c53d48623f6ea6357521
Ilari Junnonen <ilari.junnonen@iki.fi>
2021-05-31 15:44:27 +0300

    Fix ProfilePage tests

--- client/src/components/ProfilePageSeller.test.js
+++ client/src/components/ProfilePageSeller.test.js
@@ -1,10 +1,10 @@
-import { shallow/*, mount, render*/ } from 'enzyme';
+import { shallow /*, mount, render*/ } from "enzyme";
 import ProfilePageBuyer from "./ProfilePageBuyer";
 
 describe("Seller Profile", () => {
-  const wrapper = shallow(<ProfilePageBuyer/>);
+  const wrapper = shallow(<ProfilePageBuyer />);
 
   test("should render without throwing an error", () => {
-    expect(wrapper.contains(<h3 className="mb-4">Omat tiedot</h3>)).toBe(true);
+    expect(wrapper.contains(<h2 className="mb-4">Omat tiedot</h2>)).toBe(true);
   });
 });
