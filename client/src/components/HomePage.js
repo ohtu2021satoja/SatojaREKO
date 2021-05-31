078cf48fb2fd3ed6196c52f755adf7eb5468ce0b
NinaWelch <nwelch1@live.co.uk>
2021-05-31 14:55:17 +0300

    fix layout

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,14 +3,14 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
 const HomePage = ({ logOut, setSellerView }) => (
-  <Row className="d-flex align-content-center h-100 x-4">
+  <Row className="d-flex align-content-center text-center h-100">
     <Col xs={12} className="mb-4">
       <Button
         onClick={() => setSellerView(false)}
         variant="outline-primary"
         size="lg"
         type="submit"
-        className="w-100"
+        className="w-75"
       >
         Ostan lähiruokaa
       </Button>
@@ -21,13 +21,13 @@ const HomePage = ({ logOut, setSellerView }) => (
         variant="outline-primary"
         size="lg"
         type="submit"
-        className="w-100"
+        className="w-75"
       >
         Myyn lähiruokaa
       </Button>
     </Col>
     <Col xs={12} className="mb-4">
-      <Button variant="outline-success" size="lg" type="submit" className="w-100">
+      <Button variant="outline-success" size="lg" type="submit" className="w-75">
         Ota yhteyttä
       </Button>
     </Col>
@@ -37,7 +37,7 @@ const HomePage = ({ logOut, setSellerView }) => (
         variant="outline-danger"
         size="lg"
         type="submit"
-        className="w-100"
+        className="w-75"
       >
         Kirjaudu ulos
       </Button>
