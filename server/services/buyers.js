00522d9342d2d5a39965acdedc1e6a799cc2e878
ToxPuro <puro.touko2@gmail.com>
2021-06-07 16:00:48 +0300

    removed unused services

--- server/services/buyers.js
+++ server/services/buyers.js
@@ -1,13 +1,5 @@
 const BLANK_IMAGE = "profile-blank_or75kg"
 
-const updateNewsLetterCheck = async (id, check, buyersRepository) => {
-  await buyersRepository.updateNewsLetterCheck(id, check)
-}
-
-const updateCancelNotificationCheck = async (id, check, buyersRepository) => {
-  await buyersRepository.updateCancelNotificationCheck(id, check)
-}
-
 const removeBuyerImage = async (id, buyersRepository) => {
   await buyersRepository.updateBuyersImage(id, BLANK_IMAGE)
 }
