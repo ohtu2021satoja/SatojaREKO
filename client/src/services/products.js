4a612373aca2829879ae0f2a8c65fb1fb2a0e5ca
Touko Puro <74837104+ToxPuro@users.noreply.github.com>
2021-06-15 10:48:08 +0300

    Fix endpoint url

--- client/src/services/products.js
+++ client/src/services/products.js
@@ -44,7 +44,7 @@ const getUserProducts = (id) => {
 
 const addProduct = async (productObject) => {
   const response = await axios.post(
-    `${apiUrl}/${productObject.product.sellers_id}`,
+    `${apiUrl}/seller/${productObject.product.sellers_id}`,
     productObject
   )
   return response
