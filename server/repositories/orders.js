c0b8a76c820e37ed325bd6cefda5e9e23e75fcaf
ToxPuro <puro.touko2@gmail.com>
2021-06-16 11:43:22 +0300

    update orders endpoint

--- server/repositories/orders.js
+++ server/repositories/orders.js
@@ -21,7 +21,7 @@ const getSellersOrders = async (sellers_id) => {
 }
 
 const getBuyersOrders = async (buyers_id) => {
-  const query = "SELECT event_id, event_endtime, price, orders FROM (SELECT SUM(sizes.unit*products.unit_price) AS price , events.id AS event_id, events.endtime AS event_endtime, json_agg(json_build_object('quantity', batches.quantity, 'product_name', products.name, 'size', sizes.unit, 'price', sizes.unit*products.unit_price, 'type', products.type, 'unit_price', products.unit_price)) AS orders from orders INNER JOIN batches ON orders.id = batches.order_id INNER JOIN sizes ON sizes.id = batches.sizes_id INNER JOIN products ON products.id = sizes.product_id INNER JOIN buyers ON buyers.id = orders.buyers_id INNER JOIN users ON users.id = buyers.id INNER JOIN events ON events.id = orders.event_id WHERE buyers.id=$1 GROUP by (events.id, events.endtime)) AS res"
+  const query = "SELECT event_id, event_endtime, price, orders FROM (SELECT SUM(sizes.unit*products.unit_price) AS price , events.id AS event_id, events.endtime AS event_endtime, json_agg(json_build_object('quantity', batches.quantity, 'product_name', products.name, 'size', sizes.unit, 'price', sizes.unit*products.unit_price, 'type', products.type, 'unit_price', products.unit_price, 'removed', batches.removed)) AS orders from orders INNER JOIN batches ON orders.id = batches.order_id INNER JOIN sizes ON sizes.id = batches.sizes_id INNER JOIN products ON products.id = sizes.product_id INNER JOIN buyers ON buyers.id = orders.buyers_id INNER JOIN users ON users.id = buyers.id INNER JOIN events ON events.id = orders.event_id WHERE buyers.id=$1 GROUP by (events.id, events.endtime)) AS res"
   const orders = await db.query(query, [buyers_id])
   return(orders)
 }
