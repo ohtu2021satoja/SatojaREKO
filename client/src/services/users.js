3d05abe2cb29814ffb49d1b452393e4e67a8f459
NinaWelch <nwelch1@live.co.uk>
2021-06-17 11:16:31 +0300

    update facebook login

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -6,11 +6,3 @@ export const getAuthedUser = async () => {
   const response = await axios.get(`${apiUrl}/current/user`)
   return response.data
 }
-
-export const logoutUser = () => {
-  axios.put(`${apiUrl}/current/user`, null)
-}
-
-// TODO: add user
-
-// TODO: delete user
