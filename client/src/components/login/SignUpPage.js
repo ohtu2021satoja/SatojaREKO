307f611a37bf5f4ef8a3ef5113d3fa0186be94b9
NinaWelch <nwelch1@live.co.uk>
2021-06-18 09:39:38 +0300

    update back button

--- client/src/components/login/SignUpPage.js
+++ client/src/components/login/SignUpPage.js
@@ -5,18 +5,12 @@ import Button from "react-bootstrap/Button"
 import ArrowLeftIcon from "../../media/arrow-left.svg"
 import FormSignUp from "./FormSignUp"
 
-const SignUpPage = ({ user, handleSigned, handleRegisterUser }) =>  (
+const SignUpPage = ({ user, handleSigned, handleRegisterUser }) => (
   <Row className="h-100">
     <Col xs={12}>
       <Nav className="py-2">
         <Nav.Item>
-          <Button
-            variant="light"
-            size="lg"
-            className="mr-1"
-            aria-label="Return to login page"
-            onClick={handleSigned}
-          >
+          <Button variant="light" size="lg" className="mr-1" onClick={handleSigned}>
             <img src={ArrowLeftIcon} width="24" height="24" alt="Return to login" />
           </Button>
         </Nav.Item>
@@ -25,6 +19,5 @@ const SignUpPage = ({ user, handleSigned, handleRegisterUser }) =>  (
     <FormSignUp user={user} handleRegisterUser={handleRegisterUser} />
   </Row>
 )
-
 
 export default SignUpPage
