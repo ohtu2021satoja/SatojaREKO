c368dedcfe1d9886f47052bd8afde5ddb83b4f96
silmis <pate.henriksson@gmail.com>
2021-06-23 11:22:56 +0300

    -ideas folders and new email temps

--- server/services/templates/eventReminderTemp.js
+++ server/services/templates/eventReminderTemp.js
@@ -1,14 +1,54 @@
 const subject = 'Muistutus tulevasta noutotilaisuudesta'
-const message = (url) => {
+const buyerMessage = (name, event) => {
 
-    return (`<p>Paina alla olevaa vahvista nappia, vahvistaaksesi tunnuksen luomisen</p>\
+    return (`<p>Hei ${name},</p>\
     \
-    <form action=${url}>\
-        <input type="submit" value="Vahvista tunnus" />\
-    </form>`)
+    <p>Tämä on automaattisesti luotu muistutus tulevasta noutotilausuudesta</p>\
+    \
+    <p>Tilaisuus: </p>\
+    \
+    <p>market.address, event.start</p>\
+    \
+    <p>Erittely varatuista tuotteista löytyy osoitteesta:</p>\
+    \
+    <url>${profile url}</url>\
+    \
+    <p>Maukkaita hetkiä!</p>\
+    <p>Satoja Tiimi</p>\
+    \
+    \
+    <p>Etkö halua saada muistutusta tulevista noutotilaisuuksista. Voit muokata sähköpostiasetuksia profiili-sivulta</p>\
+    <p>kirjatumulalla palveluun osoitteesta:</p>
+    <url>www.satoja.fi</url>
+`)
 }
 
-module.exports = {subject, message}
+const sellerMessage = (name, event) => {
+
+    return (`<p>Hei ${name},</p>\
+    \
+    <p>Tämä on automaattisesti luotu muistutus tulevasta noutotilausuudesta</p>\
+    \
+    <p>Tilaisuus: </p>\
+    \
+    <p>market.address, event.start</p>\
+    \
+    <p>Tarkemmat tiedot varatuista tuotteista löydät osoitteesta:</p>\
+    \
+    <url>${profile url}</url>
+    
+    <p>Hyviä noutohetkiä!</p>\
+    <p>Satoja Tiimi</p>\
+    \
+    \
+    <p>Etkö halua saada muistutusta tulevista noutotilaisuuksista. Voit muokata sähköpostiasetuksia profiili-sivulta</p>\
+    <p>kirjatumulalla palveluun osoitteesta:</p>
+    <url>www.satoja.fi</url>
+    
+`)
+}
+
+module.exports = {subject, buyerMessage, sellerMessage}
 
 
     
