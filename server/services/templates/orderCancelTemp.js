3b27d022a4372252db4c79565124e43f8d83c4e3
silmis <pate.henriksson@gmail.com>
2021-06-23 11:28:44 +0300

    -ideas folders and new email temps

--- server/services/templates/orderCancelTemp.js
+++ server/services/templates/orderCancelTemp.js
@@ -1,11 +1,6 @@
 const subject = 'Tilattujen tuotteiden peruutus'
 const message = (url) => {
 
-    return (`<p>Paina alla olevaa vahvista nappia, vahvistaaksesi tunnuksen luomisen</p>\
-    \
-    <form action=${url}>\
-        <input type="submit" value="Vahvista tunnus" />\
-    </form>`)
 }
 
 module.exports = {subject, message}
