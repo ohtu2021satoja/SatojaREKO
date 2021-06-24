8e3455bfc51ba37e5d9d51027a6a022dffb4188a
ToxPuro <puro.touko2@gmail.com>
2021-06-24 11:54:51 +0300

    mgdsmpifs

--- server/index.js
+++ server/index.js
@@ -2,17 +2,18 @@ const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 const server = http.createServer(app)
+const eventsRepository = require("./repositories/events")
 
-// const functioN = async () => {
-//   const seller = await sellersRepository.getSeller(76)
-//   console.log(seller)
-// }
+  const functioN = async () => {
+  const seller = await eventsRepository.getOrderEvent(80)
+  console.log(seller)
+}
 
-// functioN()
-
-
+functioN()
 
 
-server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
-})
+
+
+// server.listen(config.PORT, () => {
+//   console.log(`Server running on port ${config.PORT}`)
+// })
