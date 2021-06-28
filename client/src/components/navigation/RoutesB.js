03dc651c67f2e53ddc96923a1505ef0380d333ec
ToxPuro <puro.touko2@gmail.com>
2021-06-28 15:58:42 +0300

    fix login bug

--- client/src/components/navigation/RoutesB.js
+++ client/src/components/navigation/RoutesB.js
@@ -2,15 +2,18 @@ import PasswordResetForm from "../PasswordResetForm"
 import { Switch, Route } from "react-router-dom"
 import LoginPage from "../login/LoginPage"
 
-const RoutesB = ({ handleLogin, handleSigned }) => (
-  <Switch>
-    <Route path exact="/">
-      <LoginPage handleLogin={handleLogin} handleSigned={handleSigned} />
-    </Route>
-    <Route path="/passwordreset">
-      <PasswordResetForm />
-    </Route>
-  </Switch>
-)
+const RoutesB = ({ handleLogin, handleSigned }) => {
+  console.log("handleLogin", handleLogin)
+  return (
+    <Switch>
+      <Route path exact="/">
+        <LoginPage handleLogin={handleLogin} handleSigned={handleSigned} />
+      </Route>
+      <Route path="/passwordreset">
+        <PasswordResetForm />
+      </Route>
+    </Switch>
+  )
+}
 
 export default RoutesB
