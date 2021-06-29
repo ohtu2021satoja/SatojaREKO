c73a16dd440309de5b99ed9688bde6e67a1e4ff5
ToxPuro <puro.touko2@gmail.com>
2021-06-29 10:46:43 +0300

    message on addproduct page if no events

--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -165,7 +165,14 @@ const ProductForm = ({
                     />
                     <p className="mb-0">Tilaus sulkeutuu {deleteBeforeEvent} tuntia</p>
                     <p>ennen noutotilaisuuden alkua</p>
-                    {events ? <Events events={events} isChoice={true} /> : null}
+                    {events.length > 0 ? (
+                      <Events events={events} isChoice={true} />
+                    ) : (
+                      <p>
+                        Reko-alueilla, joihin kuulut ei ole tulossa uusia tapahtumia. Voit
+                        lisätä itsesi uusiin Reko-alueisiin myyjän profiilista{" "}
+                      </p>
+                    )}
                   </Col>
                 </Form.Row>
               </Form.Group>
