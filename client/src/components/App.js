aa78c8cfef4f3ee78fb282081b455da2a0cf0d1b
NinaWelch <nwelch1@live.co.uk>
2021-06-29 13:46:02 +0300

    remove mock user

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,6 @@ import "../App.scss"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import LoginPage from "./login/LoginPage"
 import SignUpPage from "./login/SignUpPage"
 // import AdminPage from "./AdminPage"
 import Routes from "./navigation/Routes"
@@ -31,79 +30,6 @@ const App = (props) => {
 
   const getUser = async () => {
     const user = await getAuthedUser()
-    setAuthedUser(user)
-  }
-
-  // develoment workaroud
-  const getMockUser = () => {
-    const user = {
-      id: "75",
-      firstname: "Satoja",
-      lastname: "Reko",
-      created_at: "2021-06-21T11:57:16.859Z",
-      phonenumber: "9++043024",
-      email: "satojareko@gmail.com",
-      password: null,
-      is_buyer: false,
-      is_seller: false,
-      facebook_id: "108265444800905",
-      newsletter_check: false,
-      cancel_notification_check: false,
-      image_url: "profile-blank_or75kg",
-      name: null,
-      homepage: null,
-      address: null,
-      zipcode: null,
-      city: null,
-      salesreport_check: false,
-      description: null,
-      location: null,
-      sellers_image_url: "profile-blank_or75kg",
-      buyers_image_url: "profile-blank_or75kg",
-      reko_areas: [
-        {
-          id: 1,
-          name: "Ristiina",
-          belongs: false,
-        },
-        {
-          id: 2,
-          name: "Mikkeli",
-          belongs: false,
-        },
-        {
-          id: 3,
-          name: "Mäntyharju",
-          belongs: false,
-        },
-        {
-          id: 4,
-          name: "Puumala",
-          belongs: false,
-        },
-        {
-          id: 5,
-          name: "Pertunmaa",
-          belongs: false,
-        },
-        {
-          id: 6,
-          name: "Pieksämäki",
-          belongs: false,
-        },
-        {
-          id: 7,
-          name: "Juva",
-          belongs: false,
-        },
-        {
-          id: 8,
-          name: "testi",
-          belongs: false,
-        },
-      ],
-    }
-
     setAuthedUser(user)
   }
 
