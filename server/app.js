76518b1cdcff9446da6f5a3b1b6a90a69169502e
ToxPuro <puro.touko2@gmail.com>
2021-06-30 12:51:54 +0300

    fix refresh

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ const passportStrategy = require('./services/auth')
 app.use(express.static(path.join(__dirname, "build")))
 
 // add endpoint here if refreshing problems
-app.get(["/", "/profile", "/events", "/home", "/orders", "/add", "/cart", "/products", "/admin"], function(req, res) {
+app.get(["/", "/profile", "/events", "/home", "/orders", "/add", "/cart", "/products", "/admin","/orders/buyer", "/passwordreset", "/admin", "/update/:id", "/orders/seller", "/profile/seller", "profile/buyer", "/contact", "/map", "/events/:eventID", "/orders/buyer/:eventID", "/events/:eventID/products/:productID", "/sellers/:sellerID"], function(req, res) {
   res.sendFile(path.join(__dirname, "/build", "index.html"), function(err) {
     if (err) {
       res.status(500).send(err)
